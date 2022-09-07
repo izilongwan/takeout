5010c191e4161c880bd813518e0022e5d5a944e1
w30022785 <wanzilong3@huawei.com>
2022-09-07 16:07:52 +0800

    feat: 优化sse前端代码

--- src/main/resources/pages/sse/js/index.js
+++ src/main/resources/pages/sse/js/index.js
@@ -1,59 +1,57 @@
 ; (() => {
+  // 用时间戳模拟登录用户
+  const BASE_URL = 'http://localhost:9000/sse'
+  const userId = new Date().getTime();
   let source = null;
 
-  // 用时间戳模拟登录用户
-  const userId = new Date().getTime();
+  const init = () => {
+    if (!window.EventSource) {
+      console.log("你的浏览器不支持SSE");
+      return
+    }
 
-  if (window.EventSource) {
-
-    // 建立连接
-    source = new EventSource('http://localhost:9000/sse/sub/' + userId);
-    console.log("连接用户=" + userId);
-    /**
-     * 连接一旦建立，就会触发open事件
-     * 另一种写法：source.onopen = function (event) {}
-     */
-    source.addEventListener('open', function (e) {
-      console.log("建立连接。。。");
-    }, false);
-
-    /**
-     * 客户端收到服务器发来的数据
-     * 另一种写法：source.onmessage = function (event) {}
-     */
-    source.addEventListener('message', function (e) {
-      console.log(e.data);
-    });
-
-
-    /**
-     * 如果发生通信错误（比如连接中断），就会触发error事件
-     * 或者：
-     * 另一种写法：source.onerror = function (event) {}
-     */
-    source.addEventListener('error', function (e) {
-      if (e.readyState === EventSource.CLOSED) {
-        console.log("连接关闭");
-      } else {
-        console.log(e);
-      }
-    }, false);
-
-  } else {
-    console.log("你的浏览器不支持SSE");
+    initEventSource()
+    bindEvent()
   }
 
-  // 监听窗口关闭事件，主动去关闭sse连接，如果服务端设置永不过期，浏览器关闭后手动清理服务端数据
-  window.onbeforeunload = function () {
-    closeSse();
-  };
+  function bindEvent() {
+    source.addEventListener('open', onOpen)
+    source.addEventListener('message', onMessage)
+    source.addEventListener('error', onError)
+    // 监听窗口关闭事件，主动去关闭sse连接，如果服务端设置永不过期，浏览器关闭后手动清理服务端数据
+    window.onbeforeunload = closeSse
+  }
+
+  function initEventSource() {
+    // 建立连接
+    source = new EventSource(`${BASE_URL}/sub/${ userId }`);
+    console.log("连接用户=" + userId);
+
+    return source
+  }
+
+  function onOpen() {
+    console.log("建立连接。。。");
+  }
+
+  function onMessage(e) {
+    console.log(e.data);
+  }
+
+  function onError(e) {
+    if (e.readyState === EventSource.CLOSED) {
+      console.log("连接关闭");
+      return
+    }
+
+    console.log(e);
+  }
 
   // 关闭Sse连接
   function closeSse() {
     source.close();
-    const httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', 'http://localhost:9000/sse/disconnect/' + userId, true);
-    httpRequest.send();
+    fetch(`${BASE_URL}/disconnect/${ userId }`)
   }
 
+  init()
 })();
